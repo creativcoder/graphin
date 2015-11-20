8bfb50ee044a9840217734f403618e64043285a6
creativcoder <rsconceptx@gmail.com>
2015-11-20 23:24:58 +0530

    implementing graphin init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+extern crate sha1;
+extern crate rustc_serialize;
+
 use std::fs::OpenOptions;
 use std::io::BufReader;
 use std::io::BufWriter;
 use std::fs;
 
+use sha1::Sha1;
+
+struct Blob {
+    data:String,
+    len : usize,
+}
+
+struct Tree {
+    Head: Option<Box<Blob>>,
+    Tail: (Option<Box<Tree>> , Option<Box<Blob>> , Option<Box<Tree>>),
+}
+
+
+#[derive(RustcEncodable, RustcDecodable)]
 struct Repo {
 	status:u32,
 	files: Option<Vec<String>>,
@@ -10,26 +27,33 @@ struct Repo {
 }
 
 impl Repo {
-	fn new() -> Self {
-		Repo {status:0,files:None,dirs:None}
-	}
-}
+    fn new() -> Self {
+        Repo {status:0,files:None,dirs:None}
+    }
 
-fn init(repo_instance:&mut Repo) {
-	repo_instance.status +=1;
-    let metadata = OpenOptions::new()
+    // the graphin init cmd
+    fn init(&mut self) {
+    self.status +=1;
+    fn create_dir(name:&str) {
+        let metadata = OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
-            .open(".graphin");
+            .open(name);    
+        }
+    // create graphin metadata
+    fs::create_dir("./.graphin").unwrap();
+    create_dir(".graphin/index");
+    create_dir(".graphin/object");
     println!("Initialized an empty graphin repository");
-}
+    }
 
-fn status(repo_instance:&Repo) {
-
-	match fs::metadata(".graphin") {
+    // the graphin status cmd
+    // status command as of now checks for existing metadata to evaluate
+    fn status(&mut self) {
+    match fs::metadata(".graphin") {
         Ok(metadata) => {
-            if !(metadata.is_file()) {
+            if !(metadata.is_dir()) {
                 println!("Not a valid graphin repository");
             } else {
                 // code to add file to tracker index
@@ -39,43 +63,56 @@ fn status(repo_instance:&Repo) {
         },
         Err(_) => {println!("Not a valid graphin repository");}, 
     }
-// show status of directory here
-}
+    // show status of directory here
+    }
 
-fn add(repo_instance:&mut Repo) {
+    // the graphin add cmd
+    // TODO
+    fn add(&mut self) {
 
-    match fs::metadata(".graphin") {
+        // reads a file and returns as String
+        fn file_buffer(fd:&str) {
+
+        }
+    match fs::metadata("./.graphin") {
         Ok(metadata) => {
-            if !(metadata.is_file()) {
+            if !(metadata.is_dir()) {
                 println!("Not a valid graphin repository");
                 
             } else {
                 // code to add file to tracker index
                 // if changes in blob object, notify else    
                 let file = std::env::args().nth(2).unwrap();
-                println!("{:?} added to master branch", file);
+                // if file == '.' , add all files using walk dir
+                // Sha1 instance for hashing current file
+                let mut hasher = Sha1::new();
+                hasher.update(file.as_bytes());
+                println!("{:?} added to master branch SHA1 Hash : {:?}", file, hasher.hexdigest());
                 println!("Working directory clean", );
 
     }    
         },
         Err(_) => {println!("Not a valid graphin repository");}, 
-    }
-
-    
+    }  
 }
 
-fn branch() {
-	println!("Branches:");
-// code to add for branches
+fn branch(&mut self) {
+    // dummy repr for now
+    println!("Branch:\nOn [Master]\nLatest Commit: a24fcd2d");
+    // code to add for branches
+}
+
 }
 
 fn main() {
+    // replace the repo initialization with rustc-serialize in future
 	let mut master:Repo = Repo::new();
+
 	match &std::env::args().nth(1).unwrap()[..] {
-	"init" => {init(&mut master);},
-	"status" => status(&master),
-	"add" => add(&mut master),
-	"branch" => branch(),
+	"init" => master.init(),
+	"status" => master.status(),
+	"add" => master.add(),
+	"branch" => master.branch(),
 	_ => println!("Command Not Found"),
-}   
+    }   
 }
